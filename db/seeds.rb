93723b510c34d72287872fb6d248f738d311c4c5
kareem3m <kareem.mohamed9711@gmail.com>
2022-01-01 20:00:44 +0200

    user types seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5. times do
+25. times do
   User.create!({
     user_name: Faker::Name.unique.name,
     email: Faker::Internet.unique.email,
@@ -17,6 +17,41 @@
     birth_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
   })
 end
+
+5. times do
+  User.create!({
+    user_name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password_digest: BCrypt::Password.create(Faker::Internet.unique.password),
+    first_name: Faker::Name.unique.name,
+    last_name: Faker::Name.unique.name,
+    user_type: 'instructor',
+    birth_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
+  })
+end
+
+
+User.create!({
+  user_name: "dinaalaa",
+  email: Faker::Internet.unique.email,
+  password_digest: BCrypt::Password.create(1234),
+  first_name: "Dina",
+  last_name: "Alaa",
+  user_type: 'instructor',
+  birth_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
+})
+
+
+User.create!({
+  user_name: "kareem",
+  email: Faker::Internet.unique.email,
+  password_digest: BCrypt::Password.create(1234),
+  first_name: "Kareem",
+  last_name: "Mohamed",
+  user_type: 'admin',
+  birth_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
+})
+
 
 5. times do
   Course.create!({
@@ -31,7 +66,7 @@ end
     5 .times do
       Qa.create!({
         content: Faker::Lorem.sentence,
-        user_id: Faker::Number.between(from: 1, to: 5),
+        user_id: Faker::Number.between(from: 1, to: 25),
         course_id: random_course_id
       })
     end
@@ -42,7 +77,7 @@ end
     5 .times do
         Reply.create!({
             content: Faker::Lorem.sentence,
-            user_id: Faker::Number.between(from: 1, to: 5),
+            user_id: Faker::Number.between(from: 1, to: 30),
             qa_id: random_qa_id
         })
     end
