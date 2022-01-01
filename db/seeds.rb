aada1480dc96ae5e05045afc9a39a3d92efbd5a8
kareem3m <kareem.mohamed9711@gmail.com>
2022-01-01 19:33:20 +0200

    qa seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,63 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-    User.create!(
-        {
-            user_name: Faker::Name.unique.name,
-            email: Faker::Internet.unique.email,
-            password_digest: BCrypt::Password.create(Faker::Internet.unique.password),
-            first_name: Faker::Name.unique.name,
-            last_name: Faker::Name.unique.name,
-            user_type: 'guest',
-            birth_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
-        }
-    )
-    end
-    5.times do
-        Course.create!(
-            {
-                name: Faker::Book.title,
-                syllabus: Faker::Lorem.sentence,
-                user_id: 3
-            }
-        )
-        end
+5. times do
+  User.create!({
+    user_name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password_digest: BCrypt::Password.create(Faker::Internet.unique.password),
+    first_name: Faker::Name.unique.name,
+    last_name: Faker::Name.unique.name,
+    user_type: 'guest',
+    birth_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
+  })
+end
 
-    5.times do
-            Material.create!(
-                {
-                    material_type: 'yputube',
-                    content:'.youtube.com',
-                    course_id: 1
-                }
-            )
-            end
-            
-                5.times do
-                    Quiz.create!(
-                        {
-                            course_id: 1
-                        }
-                    )
-                    end
-                    5.times do
-                        Question.create!(
-                            {
-                                ques:'what?',
-                                answer:'yes',
-                                choices: 'yes,No,Okay',
-                                quiz_id: 1
-                            }
-                        )
-                        end
-                1.times do
-                    Takequiz.create!(
-                        {
-                            user_id: 1,
-                            quiz_id: 1,
-                            grade: 10
-                        }
-                    )
-                    end
-            
+5. times do
+  Course.create!({
+    name: Faker::Book.title,
+    syllabus: Faker::Lorem.sentence,
+    user_id: 3
+  })
+end
+
+5. times do 
+    random_course_id = Faker::Number.between(from: 1, to: 5)
+    5 .times do
+      Qa.create!({
+        content: Faker::Lorem.sentence,
+        user_id: Faker::Number.between(from: 1, to: 5),
+        course_id: random_course_id
+      })
+    end
+end
+
+25. times do
+    random_qa_id = Faker::Number.between(from: 1, to: 25)
+    5 .times do
+        Reply.create!({
+            content: Faker::Lorem.sentence,
+            user_id: Faker::Number.between(from: 1, to: 5),
+            qa_id: random_qa_id
+        })
+    end
+end
+    
+5. times do
+  Material.create!({
+    name: 'test',
+    material_type: 'yputube',
+    content: '.youtube.com',
+    course_id: 1
+  })
+end
+
+5. times do
+  Quiz.create!({
+    course_id: 1
+  })
+end
+
+5. times do
+  Question.create!({
+    ques: 'what?',
+    answer: 'yes',
+    choices: 'yes,No,Okay',
+    quiz_id: 1
+  })
+end
+
+1. times do
+  Takequiz.create!({
+    user_id: 1,
+    quiz_id: 1,
+    grade: 10
+  })
+end
